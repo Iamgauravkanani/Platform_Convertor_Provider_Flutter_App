ab05bb7de9ed445e08b6163fd42ba8224d69f60d
Gaurav Kanani <rw4.gaurav.bk@gmail.com>
2023-12-01 11:17:20 +0530

    Bottm and Modal Bottom Sheet

--- lib/Modules/App/Material_Screen/Views/material_screen.dart
+++ lib/Modules/App/Material_Screen/Views/material_screen.dart
@@ -1,15 +1,17 @@
-import 'dart:io';
+// ignore_for_file: must_be_immutable
 
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:platform_convertor_11/Modules/utils/Platform_Provider/Provider/platform_provider.dart';
 import 'package:provider/provider.dart';
 
 class Material_Screen extends StatelessWidget {
-  const Material_Screen({super.key});
-
+  Material_Screen({super.key});
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
         title: Text("Home Screen"),
         centerTitle: true,
@@ -30,8 +32,49 @@ class Material_Screen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CircularProgressIndicator(),
-            TextButton(onPressed: () {}, child: Text("Text Button")),
-            ElevatedButton(onPressed: () {}, child: Text("Material Button")),
+            TextButton(
+                onPressed: () {
+                  scaffoldKey.currentState?.showBottomSheet(
+                    (context) => Container(
+                      height: 200,
+                      width: double.infinity,
+                      color: Colors.red,
+                      alignment: Alignment.center,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            child: IconButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: Icon(Icons.camera),
+                            ),
+                          ),
+                          Container(
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.photo),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                child: Text("Show Bottom Sheet")),
+            ElevatedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: (ctx) => Container(
+                    height: 200,
+                    width: double.infinity,
+                  ),
+                );
+              },
+              child: Text("Show Modal BottomSheet"),
+            ),
           ],
         ),
       ),
